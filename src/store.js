d9f932e70288a8821105933a0c8d80bad51561b9
Juan Ramirez <ep.juan@gmail.com>
2019-10-06 19:26:39 -0500

    Correct Router

--- src/store.js
+++ src/store.js
@@ -3,8 +3,46 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// Initial State
+const userSelectedDarkMode =
+  window.localStorage.getItem("isDarkMode") === "true";
+
+const state = {
+  isDarkMode: userSelectedDarkMode
+};
+
+// Getters
+const getters = {
+  isDarkMode(state) {
+    return state.isDarkMode;
+  }
+};
+
+// Mutations
+const mutations = {
+  toggleDarkMode(state) {
+    if (state.isDarkMode === true) {
+      state.isDarkMode = false;
+      document.body.style.background = "#f0f3f5";
+      window.localStorage.setItem("isDarkMode", "false");
+    } else {
+      state.isDarkMode = true;
+      document.body.style.background = "#212c4f";
+      window.localStorage.setItem("isDarkMode", "true");
+    }
+  }
+};
+
+// Actions
+const actions = {
+  triggerDarkMode(context) {
+    context.commit("toggleDarkMode");
+  }
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  getters,
+  mutations,
+  actions
 });
